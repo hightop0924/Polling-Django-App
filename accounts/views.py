a1fe694c88f948b8b4466b206ddfe6e2691dd0f4
hightop0924 <hightop.dev.hk@gmail.com>
2023-11-11 06:05:44 +0900

    "modified on 2023-11-11"

--- accounts/views.py
+++ accounts/views.py
@@ -8,36 +8,21 @@ from django.http import HttpResponse
 
 def login_user(request):
     if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            redirect_url = request.GET.get('next', 'home')
+            return redirect(redirect_url)
+        else:
+            messages.error(request, "Username Or Password is incorrect!",
+                           extra_tags='alert alert-warning alert-dismissible fade show')
 
     return render(request, 'accounts/login.html')
 
 
-def logout_user(request):
-    logout(request)
-    return redirect('home')
-
-
-def create_user(request):
-    if request.method == 'POST':
-        check1 = False
-        check2 = False
-        check3 = False
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password1 = form.cleaned_data['password1']
-            password2 = form.cleaned_data['password2']
-            email = form.cleaned_data['email']
-
-            if password1 != password2:
-                check1 = True
-                messages.error(request, 'Password did not match!',
-                               extra_tags='alert alert-warning alert-dismissible fade show')
-            if User.objects.filter(username=username).exists():
-                check2 = True
-                messages.error(request, 'Username already exists!',
-                               extra_tags='alert alert-warning alert-dismissible fade show')
-            if User.objects.filter(email=email).exists():
                 check3 = True
                 messages.error(request, 'Email already registered!',
                                extra_tags='alert alert-warning alert-dismissible fade show')
--- polls/forms.py
+++ polls/forms.py
@@ -8,17 +8,18 @@ class PollAddForm(forms.ModelForm):
                               widget=forms.TextInput(attrs={'class': 'form-control'}))
     choice2 = forms.CharField(label='Choice 2', max_length=100, min_length=1,
                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
         model = Poll
-        fields = ['text', ]
+        fields = ['text', 'choice1', 'choice2']
         widgets = {
             'text': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'cols': 20}),
         }
 
 
-class ChoiceAddForm(forms.ModelForm):
+class EditPollForm(forms.ModelForm):
     class Meta:
-        model = Choice
-        fields = ['choice_text', ]
+        model = Poll
+        fields = ['text', ]
         widgets = {
-            'choice_text': forms.TextInput(attrs={'class': 'form-control', })
-        }
+            'text': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'cols': 20}),
